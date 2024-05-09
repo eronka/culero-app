e9d55a66c61bdf487a6c2857641c4c2430c3a516
Enache Adelina <enache.adelina99@gmail.com>
2024-05-09 11:35:40 +0300

    Add hot reload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import registerRootComponent from 'expo/build/launch/registerRootComponent';
-
-import App from './src/App';
+import registerRootComponent from "expo/build/launch/registerRootComponent";
+require("@expo/metro-runtime");
+import App from "./src/App";
 
 registerRootComponent(App);
