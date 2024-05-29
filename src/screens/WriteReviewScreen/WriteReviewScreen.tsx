bd622b152b5847708fce1ac5f4b941a5036d6c1f
Enache Adelina <enache.adelina99@gmail.com>
2024-05-29 19:51:38 +0300

    Hide header for settings

--- src/screens/WriteReviewScreen/WriteReviewScreen.tsx
+++ src/screens/WriteReviewScreen/WriteReviewScreen.tsx
@@ -9,7 +9,7 @@ import {
 } from "../../components";
 import { ConnectionReviewCard } from "../../components/ConnectionsReviewCard";
 import { useState } from "react";
-import { searchByUserLink, sendReview } from "../../utils/api";
+import { searchByUserLink } from "../../utils/api";
 import { useScreenInfo } from "../../hooks/useScreenInfo";
 import { DrawerHeader } from "../../components/headers/DrawerHeader";
 import { useQueryClient } from "@tanstack/react-query";
@@ -17,7 +17,6 @@ import { AuthType, Connection } from "../../types";
 import { Icon } from "../../icons";
 
 export const WriteReviewScreen = () => {
-  const { isPhone } = useScreenInfo();
   const queryClient = useQueryClient();
   const [searchResult, setSearchResult] = useState<{
     isInitial: boolean;
